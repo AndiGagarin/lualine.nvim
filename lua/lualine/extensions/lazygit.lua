d9a0f75be0dab13ea30a8833a3f6875853042f8f
Andreas Gahr <Andreas.GB.Gahr@bmw.de>
2025-08-02 02:39:01 +0200

    cosmetics

--- lua/lualine/extensions/lazygit.lua
+++ lua/lualine/extensions/lazygit.lua
@@ -1,31 +1,29 @@
 local M = {}
 
- M.sections = {
-    lualine_a = {
-      function()
-        return  "  " .. "lazygit"
-      end,
-    },
-    lualine_b = {},
-    lualine_c = {
-      function()
-        return vim.fn.getcwd()
-      end,
-    },
-    lualine_x = {
-      {
-        "pretty_path", -- beautify via lualine plugin's pretty path plugin's custom terminal provider plugin
-        providers = {
-          default = "terminal", -- wont encounter anything else inside this extension
-        },
+M.sections = {
+  lualine_a = {
+    function()
+      return '  ' .. 'lazygit'
+    end,
+  },
+  lualine_b = {},
+  lualine_c = {
+    function()
+      return vim.fn.getcwd()
+    end,
+  },
+  lualine_x = {
+    {
+      'pretty_path', -- beautify via lualine plugin's pretty path plugin's custom terminal provider plugin
+      providers = {
+        default = 'terminal', -- wont encounter anything else inside this extension
       },
     },
-    lualine_y = {},
-    lualine_z = {},
+  },
+  lualine_y = {},
+  lualine_z = {},
 }
 
-M.filetypes = { "lazygit" }
+M.filetypes = { 'lazygit' }
 
 return M
-
-
--- lua/lualine/extensions/shell.lua
+++ lua/lualine/extensions/shell.lua
@@ -1,37 +1,35 @@
-
 local M = {}
 
 M.sections = {
-    lualine_a = {
-      {
-        "mode",
-        fmt = function(str)
-          return "  " .. str:sub(1, 1)
-        end,
+  lualine_a = {
+    {
+      'mode',
+      fmt = function(str)
+        return str:sub(1, 1)
+      end,
+    },
+  },
+  lualine_b = {},
+  lualine_c = {
+    {
+      'pretty_path', -- beautify via lualine plugin's pretty path plugin's custom terminal provider plugin
+      providers = {
+        'shell', -- wont encounter anything else inside this extension
       },
     },
-    lualine_b = {},
-    lualine_c = {
-      {
-        "pretty_path", -- beautify via lualine plugin's pretty path plugin's custom terminal provider plugin
-        providers = {
-          "shell", -- wont encounter anything else inside this extension
-        },
+  },
+  lualine_x = {
+    {
+      'pretty_path', -- beautify via lualine plugin's pretty path plugin's custom terminal provider plugin
+      providers = {
+        'terminal', -- wont encounter anything else inside this extension
       },
     },
-    lualine_x = {
-      {
-        "pretty_path", -- beautify via lualine plugin's pretty path plugin's custom terminal provider plugin
-        providers = {
-          "terminal", -- wont encounter anything else inside this extension
-        },
-      },
-    },
-    lualine_y = {},
-    lualine_z = {},
+  },
+  lualine_y = {},
+  lualine_z = {},
 }
 
-M.filetypes = { "shell" }
+M.filetypes = { 'shell' }
 
 return M
-
